ca633f51060b74bc3f4609742bff11018babdd7a
BjörnFalkDRT <43184107+BjornFalkDRT@users.noreply.github.com>
2023-10-28 15:13:37 +0200

    Done by me

--- EDA.jl
+++ EDA.jl
@@ -8,7 +8,39 @@ using PlotlyBase
 
 const data = CSV.read("data/HousingData.csv", DataFrame, missingstring="NA") |> DataFrame
 
-@app begin end
+@app begin
+    # Scatter Plot
+    @out scatter_trace = [scatter(
+        x=data[:, "RM"],
+        y=data[:, "MEDV"],
+        mode="markers",
+        marker=attr(size=10, color="rgba(255, 182, 193, .9)"),
+        name="Rooms vs MEDV"
+    )]
+    @out scatter_layout = PlotlyBase.Layout(
+        xaxis_title="Rooms",
+        yaxis_title="MEDV"
+    )
+
+    # Histogram
+    @out hist_trace = [histogram(x=data[:, "MEDV"], name="Median Value Distribution")]
+    @out hist_layout = PlotlyBase.Layout(
+        xaxis_title="MEDV",
+        yaxis_title="Count"
+        )
+
+    # Box Plot
+    @out box_trace = [box(y=data[:, "MEDV"], name="Boston")]
+    @out box_layout = PlotlyBase.Layout(
+        yaxis_title="MEDV"
+        )
+
+    #Data table
+    @out housing_data = DataTable(data)
+    @out pagination = DataTablePagination(
+            rows_per_page=10)
+
+end
 
 @page("/", "EDA_ui.jl")
 end
